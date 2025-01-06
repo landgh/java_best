05449ebaf445489a02d7fba05cf3d7756e6c5e26
land gh <landghgh@gmail.com>
2025-01-06 15:33:31 -0500

    update git util

--- src/main/resources/scripts/git_util.sh
+++ src/main/resources/scripts/git_util.sh
@@ -19,7 +19,7 @@ refresh_repos() {
     echo "Refreshing git repos with $@"
     for p in "${!projs[@]}"; do
         dir=${projs[$p]}
-        branch=${branches[$p]:-master}
+        branch=${branches[$p]:-$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')}
         ok=0
 
         if [ "$param" ] && [ "$param" = "$p" ]; then
